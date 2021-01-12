385ffe72b71f9d9076ac9dc6135376a18a5ea8c9
sidecodes <sidekicktal@outlook.com>
2021-01-12 22:22:32 +0530

    Completed CRUD operations

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import '../css/App.css';
 import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
-import { without } from 'lodash';
+import { findIndex, without } from 'lodash';
 
 
 class App extends Component {
@@ -11,10 +11,58 @@ class App extends Component {
     super();
     this.state = {
       myAppointments: [],
-      lastIndex: 0
+      formDisplay: false,
+      lastIndex: 0,
+      orderBy: 'heroName',
+      orderDir: 'asc',
+      queryText: ''
     };
     
     this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.addAppointment = this.addAppointment.bind(this);
+    this.changeOrder = this.changeOrder.bind(this);
+    this.searchAppointments = this.searchAppointments.bind(this);
+    this.updateInfo = this.updateInfo.bind(this);
+  }
+
+  searchAppointments(query) {
+    this.setState({ queryText: query });
+  }
+
+  changeOrder(order, dir) {
+    this.setState({
+      orderBy: order,
+      orderDir: dir
+    });
+  }
+
+  updateInfo(name, value, id) {
+    let tempAppointments = this.state.myAppointments;
+    let aptIndex = findIndex(this.state.myAppointments, {
+      aptId: id
+    });
+    tempAppointments[aptIndex][name] = value;
+    this.setState({
+      myAppointments: tempAppointments
+    });
+  }
+
+  toggleForm() {
+    this.setState({
+      formDisplay: !this.state.formDisplay
+    });
+  }
+
+
+  addAppointment(apt) {
+    let tempAppointments = this.state.myAppointments;
+    apt.aptId = this.state.lastIndex;
+    tempAppointments.unshift(apt);
+    this.setState({
+      myAppointments: tempAppointments,
+      lastIndex: this.state.lastIndex + 1
+    });
   }
 
   deleteAppointment(apt) {
@@ -42,16 +90,58 @@ class App extends Component {
   }
 
   render() {
+    let order;
+    let filteredApts = this.state.myAppointments;
+    if (this.state.orderDir === 'asc') {
+      order = 1;
+    } else {
+      order = -1;
+    }
+
+    filteredApts = filteredApts
+      .sort((a, b) => {
+        if (
+          a[this.state.orderBy].toLowerCase() < 
+          b[this.state.orderBy].toLowerCase()
+        ) {
+          return -1 * order;
+        } else {
+          return 1 * order;
+        }
+      })
+      .filter(eachItem => {
+        return (
+          eachItem['heroName']
+            .toLowerCase()
+            .includes(this.state.queryText.toLowerCase()) ||
+          eachItem['ownerName']
+            .toLowerCase()
+            .includes(this.state.queryText.toLowerCase()) ||
+          eachItem['aptNotes']
+            .toLowerCase()
+            .includes(this.state.queryText.toLowerCase())
+        );
+      });
+    
     return (
       <main className="page bg-white" id="heroratings">
         <div className="container">
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <AddAppointments />
-                <SearchAppointments />
+                <AddAppointments 
+                  formDisplay={this.state.formDisplay} 
+                  toggleForm={this.toggleForm}
+                  addAppointment={this.addAppointment}
+                />
+                <SearchAppointments 
+                  orderBy={this.state.orderBy}
+                  orderDir={this.state.orderDir}
+                  changeOrder={this.changeOrder}
+                  searchAppointments={this.searchAppointments}
+                />
                 <ListAppointments 
-                  appointments={this.state.myAppointments}
+                  appointments={filteredApts}
                   deleteAppointment={this.deleteAppointment} 
                 />
               </div>
