d7ba8aa7ef92e16a48845533bf5bca184bc66214
sidecodes <sidekicktal@outlook.com>
2021-01-12 17:51:45 +0530

    Updated index

---
+++ src/components/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import '../css/App.css';
+import AddAppointments from './AddAppointments';
+import SearchAppointments from './SearchAppointments';
+import ListAppointments from './ListAppointments';
+import { without } from 'lodash';
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myAppointments: [],
+      lastIndex: 0
+    };
+    
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+  }
+
+  deleteAppointment(apt) {
+    let tempAppointments = this.state.myAppointments;
+    tempAppointments = without(tempAppointments, apt);
+
+    this.setState({
+      myAppointments: tempAppointments
+    });
+  }
+
+  componentDidMount() {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          item.aptId = this.state.lastIndex;
+          this.setState({ lastIndex: this.state.lastIndex + 1});
+          return item;
+        });
+        this.setState({
+          myAppointments: apts
+        });
+      });
+  }
+
+  render() {
+    return (
+      <main className="page bg-white" id="heroratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments 
+                  appointments={this.state.myAppointments}
+                  deleteAppointment={this.deleteAppointment} 
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
